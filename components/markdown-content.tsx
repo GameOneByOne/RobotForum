7740bc6c15d3ce3f462590bf99d8df88c522490e
HersonZhang <yanzhang.a2@gmail.com>
2026-09-02 23:10:51 +0800

    :rocket: 修复vercel部署失败的问题  Please enter the commit message for your changes. Lines starting

--- components/markdown-content.tsx
+++ components/markdown-content.tsx
@@ -368,7 +368,7 @@ function MarkdownRenderer({ source }: MarkdownContentProps) {
         ),
       }}
     >
-      {preserveSingleLineBreaks(source)}
+      {source}
     </ReactMarkdown>
   );
 }
